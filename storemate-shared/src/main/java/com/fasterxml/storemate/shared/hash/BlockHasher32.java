45cd9019d1a60a338ceb048c83d1e6cf7db00cdc
Tatu Saloranta <tsaloranta@salesforce.com>
2012-09-17 17:11:01 -0700

    ...

--- storemate-shared/src/main/java/com/fasterxml/storemate/shared/hash/BlockHasher32.java
+++ storemate-shared/src/main/java/com/fasterxml/storemate/shared/hash/BlockHasher32.java
@@ -3,9 +3,17 @@ package com.fasterxml.storemate.shared.hash;
 public abstract class BlockHasher32
 {
     public final static int DEFAULT_SEED = 0;
+
+    public final int hash(byte[] data) {
+        return hash(DEFAULT_SEED, data, 0, data.length);
+    }
     
     public int hash(int seed, byte[] data) {
         return hash(seed, data, 0, data.length);
+    }
+
+    public final int hash(byte[] data, int offset, int len) {
+        return hash(DEFAULT_SEED, data, offset, len);
     }
 
     public abstract int hash(int seed, byte[] data, int offset, int len);
