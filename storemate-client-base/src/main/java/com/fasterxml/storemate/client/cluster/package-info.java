57fa4c89d7ac969e98f83adcbb6463aa502bc9ea
Tatu Saloranta <tsaloranta@gmail.com>
2012-10-19 16:32:51 -0700

    move stuff

--- storemate-client-base/src/main/java/com/fasterxml/storemate/client/cluster/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Package that contains client-side abstractions that deal
- * with state of server, including rudimentary information
- * about possible server clusters.
- */
-package com.fasterxml.storemate.client.cluster;
-
--- storemate-client-base/src/main/java/com/fasterxml/storemate/client/operation/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Package that contains abstractions used for handling of logical
- * operations that may consist of multiple physical calls.
- */
-package com.fasterxml.storemate.client.operation;
-
