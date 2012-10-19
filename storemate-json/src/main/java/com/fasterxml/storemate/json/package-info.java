f4e9e5f4f230aa8f9b604a31b380998b498371af
Tatu Saloranta <tsaloranta@gmail.com>
2012-10-19 16:46:07 -0700

    Moving JSON stuff out as well

--- storemate-json/src/main/java/com/fasterxml/storemate/json/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Package that contains Jackson converters, primarily for reading/writing
- * data as JSON, but also applicable to other formats Jackson supports.
- */
-package com.fasterxml.storemate.json;
--- storemate-json/src/test/java/com/fasterxml/storemate/json/JsonTestBase.java
+++
@@ -1,7 +0,0 @@
-package com.fasterxml.storemate.json;
-
-import com.fasterxml.storemate.shared.SharedTestBase;
-
-public class JsonTestBase extends SharedTestBase
-{
-}
