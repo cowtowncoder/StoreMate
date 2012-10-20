f8cedf96d099dc6f875cde62659f5bcc7ae97b9f
Tatu Saloranta <tsaloranta@gmail.com>
2012-10-19 19:55:29 -0700

    promote things to clustermate

--- storemate-api/src/main/java/com/fasterxml/storemate/api/package-info.java
+++ storemate-api/src/main/java/com/fasterxml/storemate/api/package-info.java
@@ -2,8 +2,8 @@
  * Package that contains high-level abstractions needed by
  * indivdiual instances of actual storage service servers and clients.
  * This includes key space handling pieces like
- * {@link com.fasterxml.storemate.api.KeySpace}
- * and {@link com.fasterxml.storemate.api.KeyRange}, as well as
+ * {@link com.fasterxml.clustermate.api.KeySpace}
+ * and {@link com.fasterxml.clustermate.api.KeyRange}, as well as
  * basic node definitions and constants.
  */
 package com.fasterxml.storemate.api;
