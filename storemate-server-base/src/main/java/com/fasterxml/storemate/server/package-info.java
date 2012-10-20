cf27620c52d6a41245f10e886c99a469400bc9a8
Tatu Saloranta <tsaloranta@gmail.com>
2012-10-19 20:03:22 -0700

    remove server-base package

--- storemate-server-base/src/main/java/com/fasterxml/storemate/server/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Package contains basic building that are useful for building
- * actual services around StoreMate store implementation.
- * Components expose mostly single-node functionality, but also
- * lay groundwork for some aspects of possible clustered implementation.
- */
-package com.fasterxml.storemate.server;
-
