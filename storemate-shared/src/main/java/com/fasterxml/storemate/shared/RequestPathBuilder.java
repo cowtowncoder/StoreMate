c31cd7d5d018d6ccc5a4ff5fc3a07be0345a480e
Tatu Saloranta <tsaloranta@gmail.com>
2012-10-25 16:42:05 -0700

    Adding javadoc comments

--- storemate-shared/src/main/java/com/fasterxml/storemate/shared/RequestPathBuilder.java
+++ storemate-shared/src/main/java/com/fasterxml/storemate/shared/RequestPathBuilder.java
@@ -6,9 +6,27 @@ package com.fasterxml.storemate.shared;
  */
 public abstract class RequestPathBuilder
 {
-	public abstract RequestPathBuilder addPathSegment(String segment);
+    /**
+     * Method that will append a single path segment, escaping characters
+     * as necessary.
+     *
+     * @return Builder instance to use for further calls (may be 'this',
+     *   but does not have to be)
+     */
+    public abstract RequestPathBuilder addPathSegment(String segment);
 	
-	public abstract RequestPathBuilder addParameter(String key, String value);
-	
-	public abstract RequestPath build();
+    /**
+     * Method that will add a single query parameter in the logical
+     * path, with given value.
+     *
+     * @return Builder instance to use for further calls (may be 'this',
+     *   but does not have to be)
+     */
+    public abstract RequestPathBuilder addParameter(String key, String value);
+
+    /**
+     * Method that will construct the immutable {@link RequestPath} instance
+     * with information builder has accumulated.
+     */
+    public abstract RequestPath build();
 }
