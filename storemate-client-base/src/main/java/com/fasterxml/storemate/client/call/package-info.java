1113d243503e74adae85827528b36ed59b6c7b38
Tatu Saloranta <tsaloranta@gmail.com>
2013-01-25 13:20:14 -0800

    Refactoring: moving base-client part out; does not really belong here

--- storemate-client-base/src/main/java/com/fasterxml/storemate/client/call/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Package that contains abstractions used for handling of a
- * single physical call by client to server.
- * These are done in context of an operation; one or more calls
- * for each operation.
- */
-package com.fasterxml.storemate.client.call;
-
